3e23dff2b6a7fc541cc53acdd312681bdef2c7d7
vinhSalescore <danghoangnguyenvinh@salescore.me>
2025-10-15 08:34:19 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${luckiestGuy.variable} antialiased overflow-x-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} ${luckiestGuy.variable} scroll-smooth antialiased overflow-x-hidden`}
       >
         {children}
       </body>
